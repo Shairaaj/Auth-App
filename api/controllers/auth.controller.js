c12c05e92bd5d914649920a07154cf3e72d793df
Shairaaj <iamshairaaj@gmail.com>
2025-07-21 21:45:29 +0530

    Completed Sign-in functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,39 @@
 import User from "../models/user.model.js";
-import bcryptjs from "bcryptjs"
-import { errorHander } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const signup =async (req,res,next) =>{
-    const {username, email, password} =req.body;
-    const hashedPassword= await bcryptjs.hash(password,10);
-    const newUser= new User({username, email, password: hashedPassword});
-    try{
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  try {
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
-    res.status(201).json({message: "User created successfully",success: true});
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
     }
-    catch(err){
-        next(errorHander(300,res.message));
+    const validPassword = await bcryptjs.compare(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(402, "Invalid Credentials"));
     }
-}
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, email:_, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now()+86400000);
+    res
+      .cookie("access_token", token, { httpOnly: true, expires:expiryDate})
+      .status(200)
+      .json(rest);
+  } catch (err) {
+    next(err);
+  }
+};
